e8937a039e77d61746092a7a788cfffdb7f71005
SimoneZoroddu <lenviur@gmail.com>
2026-06-12 23:13:54 +0200

    Implement Prodotto class with attributes and getter/setter methods

--- src/org/lessons/java/oop/Prodotto.java
+++ src/org/lessons/java/oop/Prodotto.java
@@ -1,5 +1,60 @@
 package org.lessons.java.oop;
 
+import java.math.BigDecimal;
+
 public class Prodotto {
     
+    protected int codice;
+    protected String nome;
+    protected String marca;
+    protected BigDecimal prezzo;
+    protected BigDecimal iva;
+
+    public Prodotto(int codice, String nome, String marca, BigDecimal prezzo, BigDecimal iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.marca = marca;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public int getCodice() {
+        return this.codice;
+    }
+
+    public void setCodice(int codice) {
+        this.codice = codice;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public BigDecimal getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public BigDecimal getIva() {
+        return this.iva;
+    }
+
+    public void setIva(BigDecimal iva) {
+        this.iva = iva;
+    }
 }
